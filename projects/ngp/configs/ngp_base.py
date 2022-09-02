cb9012666b528e7cbf7f7aaf26e69b0b771a0315
jiayaozhang <jiayao.zhang@kaust.edu.sa>
2022-09-02 12:22:14 +0800

    change ngp for dnerf

--- projects/ngp/configs/ngp_base.py
+++ projects/ngp/configs/ngp_base.py
@@ -36,51 +36,49 @@ expdecay=dict(
     decay_end=None
 )
 dataset_type = 'NerfDataset'
-dataset_dir = 'data/lego'
+dataset_dir = 'data/jumpingjacks'
+n_rays_per_batch = 1024
 dataset = dict(
     train=dict(
         type=dataset_type,
         root_dir=dataset_dir,
-        batch_size=4096,
+        batch_size=n_rays_per_batch,
         mode='train',
     ),
     val=dict(
         type=dataset_type,
         root_dir=dataset_dir,
-        batch_size=4096,
+        batch_size=n_rays_per_batch,
         mode='val',
         preload_shuffle=False,
     ),
     test=dict(
         type=dataset_type,
         root_dir=dataset_dir,
-        batch_size=4096,
+        batch_size=n_rays_per_batch,
         mode='test',
         preload_shuffle=False,
     ),
 )
 
-exp_name = "lego"
-log_dir = "./logs"
-tot_train_steps = 40000
+exp_name = "jumpingjacks"
+log_dir = "./jumpingjacks"
+tot_train_steps = 80000
 # Background color, value range from 0 to 1
 background_color = [0, 0, 0]
 # Hash encoding function used in Instant-NGP
 hash_func = "p0 ^ p1 * 19349663 ^ p2 * 83492791"
 cone_angle_constant = 0.00390625
 near_distance = 0.2
-n_rays_per_batch = 4096
 n_training_steps = 16
 # Expected number of sampling points per batch
-target_batch_size = 1<<18
+target_batch_size = 1<<10
 # Set const_dt=True for higher performance
 # Set const_dt=False for faster convergence
 const_dt=True
 # Use fp16 for faster training
-fp16 = True
+fp16 = False
 # Load pre-trained model
 load_ckpt = False
 # path of checkpoint file, None for default path
 ckpt_path = None
-# test output image with alpha
-alpha_image= False
